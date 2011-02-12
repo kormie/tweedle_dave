ad160810f4361cb8d839e7782f95f82ea66da401
David Kormushoff <david@jotlocker.com>
2011-02-12 17:09:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweedleDave::Application.config.secret_token = 'f1ba821ce10d30b6b0d5e64a5e403a09a9046a4fab94eb34605257e427d9adc2c9d4cd19eb4b3fee1dda6d7e9e88ef1d595abd806f13d1181ab02070b5374b70'
