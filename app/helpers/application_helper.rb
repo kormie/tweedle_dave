7e7df5ca7cc16cb1dd2c468dee41db0160a07d07
David Kormushoff <david@jotlocker.com>
2011-02-12 18:56:24 -0600

    Added initial structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def ie7png
+    raw("<!--[if lt IE 7 ]>
+      <script src='js/libs/dd_belatedpng.js'></script>
+      <script> DD_belatedPNG.fix('img, .png_bg'); </script>
+    <![endif]-->")
+  end
 end
